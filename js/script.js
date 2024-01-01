93dc17dc91eaadd36f29dcb29ee61d9736342d1a
Akshita Sharma <151676076+sharma-akshita@users.noreply.github.com>
2024-01-01 15:33:06 -0500

    Updated Portfolio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var links = document.getElementsByClassName("links");
+var contents = document.getElementsByClassName("contents");
+
+function opentab(name){
+    for(link of links){
+        link.classList.remove("active-link");
+    }
+    for(content of contents){
+        content.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(name).classList.add("active-tab");
+}
