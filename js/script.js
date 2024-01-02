cd33dbe1f340a65726823a9b7dca3538a3220e9b
Akshita Sharma <151676076+sharma-akshita@users.noreply.github.com>
2024-01-02 16:44:36 -0500

    Modified Contact Section

--- js/script.js
+++ js/script.js
@@ -18,3 +18,33 @@ function opentab(name){
     event.currentTarget.classList.add("active-link");
     document.getElementById(name).classList.add("active-tab");
 }
+
+/*
+const form = document.querySelector('form');
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+function sendEmail(){
+
+    const body = `Name: ${name.value}<br> Email: ${email.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "theradiantedge@gmail.com",
+        Password : "3068D1EE01954FA1CA4360A2613DE8AB3006",
+        To : 'theradiantedge@gmail.com',
+        From : "theradiantedge@gmail.com",
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        sendEmail();
+    });
+}
+*/
